7bb1626c6dd4f6dc8d97bd3c9c13356d1b877af2
Kajal Gupta <Kajal.Gupta1.@natwest.com>
2024-03-28 22:45:22 +0530

    Add new feature

--- src/main/java/com/natwest/loanservice/entity/Loan.java
+++ src/main/java/com/natwest/loanservice/entity/Loan.java
@@ -1,9 +1,15 @@
 package com.natwest.loanservice.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "Loans Details")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Loan {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,83 +21,4 @@ public class Loan {
     private int EMI_Amount;
     private String status;
 
-    public Loan() {
-    }
-
-    public Loan(int loanId, double loanAmount, int duration, String loanType, double loanInterestRate, int numberOfPayment, String status) {
-        this.loanId = loanId;
-        this.loanAmount = loanAmount;
-        this.duration = duration;
-        this.loanType = loanType;
-        this.loanInterestRate = loanInterestRate;
-        this.EMI_Amount = numberOfPayment;
-        this.status = status;
-    }
-
-    public int getLoanId() {
-        return loanId;
-    }
-
-    public void setLoanId(int loanId) {
-        this.loanId = loanId;
-    }
-
-    public double getLoanAmount() {
-        return loanAmount;
-    }
-
-    public void setLoanAmount(double loanAmount) {
-        this.loanAmount = loanAmount;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public String getLoanType() {
-        return loanType;
-    }
-
-    public void setLoanType(String loanType) {
-        this.loanType = loanType;
-    }
-
-    public double getLoanInterestRate() {
-        return loanInterestRate;
-    }
-
-    public void setLoanInterestRate(double loanInterestRate) {
-        this.loanInterestRate = loanInterestRate;
-    }
-
-    public int getEMI_Amount() {
-        return EMI_Amount;
-    }
-
-    public void setEMI_Amount(int EMI_Amount) {
-        this.EMI_Amount = EMI_Amount;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "Loan{" +
-                "loanId='" + loanId + '\'' +
-                ", loanAmount=" + loanAmount +
-                ", duration=" + duration +
-                ", loanType='" + loanType + '\'' +
-                ", loanInterestRate=" + loanInterestRate +
-                '}';
-    }
 }
